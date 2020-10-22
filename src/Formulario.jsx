842ece95b2f939c27c567b3623d6c7caad8a21cd
nicolasbica <swordk664@gmail.com>
2020-10-22 16:31:44 -0300

    mod-formulario

---
+++ src/Formulario.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+
+function Formulario() {
+    const [pessoa, alteraPessoa] = useState({})
+
+    function onChangeUm(event) {
+        alteraPessoa({
+            ...pessoa,
+            nome: event.target.value
+        })
+    }
+
+    function onChangeDois(event) {
+        alteraPessoa({
+            ...pessoa,
+            idade: event.target.value
+        })
+    }
+
+    function onChangeTres(event) {
+        alteraPessoa({
+            ...pessoa,
+            telefone: event.target.value
+        })
+    }
+
+    function onChangeQuatro(event){
+        alteraPessoa({
+       ...pessoa,
+            cidade: event.target.value
+        })
+    }
+
+    function onChangeCinco(event){
+        alteraPessoa({
+            ...pessoa,
+            email: event.target.value
+        })
+    }
+
+    function onChangeSeis(event){
+        alteraPessoa({
+            emprego: event.target.checked,
+        })
+    }
+
+    function onChangeSete(event){
+        alteraPessoa({
+            ...pessoa,
+            dataNascimento: event.target.value
+        })
+    }
+
+    return <form> 
+        <input onChange={onChangeUm} type="text" name="nome" id="1" />
+            <input onChange={onChangeDois} type="number" name="idade" id="2" />
+                <input onChange={onChangeTres} type="number" name="telefone" id="3" />
+                    <input onChange={onChangeQuatro} type="text" name="cidade" id="4" />
+                        <input onChange={onChangeCinco} type="email" name="email" id="5" />
+                            <input onChange={onChangeSeis} type="checkbox" name="emprego" id="6" />
+                                <input onChange={onChangeSete} type="text" name="dataNascimento" id="7" />
+
+
+        <p>Nome {pessoa.nome}</p>
+            <p>Idade {pessoa.idade}</p>
+                <p>Telefone {pessoa.telefone}</p>
+                    <p>Cidade {pessoa.cidade}</p>
+                        <p>Email {pessoa.email}</p>
+                            <p>Nasci {pessoa.dataNascimento}</p>
+
+    </form>
+}
+
+export default Formulario
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Formulario from './Formulario'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Formulario />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
