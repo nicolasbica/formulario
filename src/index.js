9a8a09232caddd554daeeb2b12f78b8c7cd22dd0
nicolasbica <swordk664@gmail.com>
2020-10-26 21:33:49 -0300

    mod

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Formulario from './Formulario'
+import Formulario from './main/Formulario'
+
 ReactDOM.render(
   <React.StrictMode>
     <Formulario />
+    <curriculo />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/main/curriculo.jsx
+++ src/main/curriculo.jsx
@@ -19,8 +19,4 @@ function curriculo () {
   const usuario = getFromLocalStorage('usuario')
   const [usuario, alteraPessoa] = useState(usuario || {})
 
-<div>
-  <h1> Oi </h1>
-</div>
-
 export default curriculo
